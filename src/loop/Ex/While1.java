2757a642f9cecbf40f797f316e302dde63304186
SooYoung <dearsyjang@naver.com>
2024-02-21 23:55:15 +0900

    [feat] 반복문2

---
+++ src/loop/Ex/While1.java
@@ -0,0 +1,11 @@
+package loop.Ex;
+
+public class While1 {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count < 11) {
+            System.out.println(count);
+            count ++;
+        }
+    }
+}
